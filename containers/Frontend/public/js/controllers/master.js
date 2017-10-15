c76c1b45a5ecdebc03eebbce98d3cb79a62c0e97
TropicalBastos <ian-bastos@live.com>
2017-10-15 02:40:44 +0100

    more angular integration

--- containers/Frontend/public/js/controllers/master.js
+++ containers/Frontend/public/js/controllers/master.js
@@ -1,10 +1,24 @@
 var app = angular.module('betaway', []);
 var endpoint = ""
 
-app.controller('master', ['$scope', '$http', function($scope, $http){
+app.controller('master', ['$scope', '$http', 'betFlight', 
+    function($scope, $http, betFlight){
 
     $scope.dest = "";
     $scope.price = "£0.00";
     $scope.processing = false;
+    $scope.modalOn = false;
+
+    $scope.confirmGet = function(){
+        $scope.modalOn = true;
+    };
+
+    $scope.getData = function(){
+        betFlight.getData($scope.dest);
+    }
+
+    $scope.cancel = function(){
+        $scope.modalOn = false;
+    }
 
 }]);
